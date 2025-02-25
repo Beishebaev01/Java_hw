fc06875e482b4e71588833479b0754330cbd83fd
Beishebaev01 <bshv228@icloud.com>
2025-02-25 10:14:36 +0600

    deleted comments

--- src/Lab2Task1.java
+++ src/Lab2Task1.java
@@ -6,7 +6,6 @@ public class Lab2Task1 {
         System.out.print("Введите строку: ");
         String input = scanner.nextLine();
 
-        // Массив согласных букв (английский и русский алфавиты)
         String[] consonants = {
                 "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
                 "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",
@@ -14,28 +13,24 @@ public class Lab2Task1 {
                 "Б", "В", "Г", "Д", "Ж", "З", "Й", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "Ф", "Х", "Ц", "Ч", "Ш", "Щ"
         };
 
-        char firstConsonant = '\0'; // Переменная для хранения первого согласного символа
+        char firstConsonant = '\0';
 
-        // Проходим по каждому символу в строке
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
-            String currentCharStr = String.valueOf(currentChar); // Преобразуем символ в строку
+            String currentCharStr = String.valueOf(currentChar);
 
-            // Проверяем, содержится ли символ в массиве согласных
             for (String consonant : consonants) {
                 if (consonant.equals(currentCharStr)) {
-                    firstConsonant = currentChar; // Найден первый согласный
+                    firstConsonant = currentChar;
                     break;
                 }
             }
 
-            // Если найден первый согласный, выходим из цикла
             if (firstConsonant != '\0') {
                 break;
             }
         }
 
-        // Выводим результат
         if (firstConsonant != '\0') {
             System.out.println("Первый согласный символ в строке: " + firstConsonant);
         } else {
--- src/Lab2Task4.java
+++ src/Lab2Task4.java
@@ -2,14 +2,11 @@ public class Lab2Task4 {
     public static void main(String[] args) {
         String text = "This is a very big house with a very big garden.";
 
-        // Вызов метода для замены
         String result = replaceVeryBig(text);
 
-        // Вывод результата
         System.out.println(result);
     }
 
-    // Метод для замены "very big" на "enormous"
     public static String replaceVeryBig(String str) {
         return str.replace("very big", "enormous");
     }
