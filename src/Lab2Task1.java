03a5ed7471ca9bc3e68cf0a8e910514a5f35a345
Beishebaev01 <bshv228@icloud.com>
2025-02-25 10:47:09 +0600

    Changed task 1

--- src/Lab2Task1.java
+++ src/Lab2Task1.java
@@ -6,35 +6,15 @@ public class Lab2Task1 {
         System.out.print("Введите строку: ");
         String input = scanner.nextLine();
 
-        String[] consonants = {
-                "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
-                "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",
-                "б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ",
-                "Б", "В", "Г", "Д", "Ж", "З", "Й", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "Ф", "Х", "Ц", "Ч", "Ш", "Щ"
-        };
-
-        char firstConsonant = '\0';
-
+        String consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ";
+        
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
-            String currentCharStr = String.valueOf(currentChar);
-
-            for (String consonant : consonants) {
-                if (consonant.equals(currentCharStr)) {
-                    firstConsonant = currentChar;
-                    break;
-                }
-            }
-
-            if (firstConsonant != '\0') {
-                break;
+            if (consonants.indexOf(currentChar) != -1) {
+                System.out.println("Первый согласный символ в строке: " + currentChar);
+                return;
             }
         }
-
-        if (firstConsonant != '\0') {
-            System.out.println("Первый согласный символ в строке: " + firstConsonant);
-        } else {
-            System.out.println("В строке нет согласных символов.");
-        }
+        System.out.println("В строке нет согласных символов.");
     }
 }
