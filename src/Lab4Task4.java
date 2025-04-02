c846cec6b4a59c805c854d93450f3a378e338d08
Beishebaev01 <bshv228@icloud.com>
2025-04-02 09:28:13 +0600

    Delete comments

--- src/Lab4Task4.java
+++ src/Lab4Task4.java
@@ -5,36 +5,17 @@ import java.util.Set;
 
 public class Lab4Task4 {
     public static void main(String[] args) {
-//        String text = "Hello, World! My name is Argen!";
-//        System.out.println("Анализ строки: " + text);
-//
-//        // Частотный анализ символов
-//        Map<Character, Integer> charFrequency = new HashMap<>();
-//        for (char c : text.toCharArray()) {
-//            charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
-//        }
-//        System.out.println("Частота символов: " + charFrequency);
-//
-//        // Частотный анализ слов
-//        String[] words = text.split("\\W+");
-//        Map<String, Integer> wordFrequency = new HashMap<>();
-//        for (String word : words) {
-//            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
-//        }
-//        System.out.println("Частота слов: " + wordFrequency);
         String text1 = "Hello, Argen! My name is Argen!";
         String text2 = "Being Argen is cool!";
 
         System.out.println("Анализ строки: " + text1);
 
-        // Частотный анализ символов первой строки
         Map<Character, Integer> charFrequency = new HashMap<>();
         for (char c : text1.toCharArray()) {
             charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
         }
         System.out.println("Частота символов: " + charFrequency);
 
-        // Частотный анализ слов первой строки
         String[] words = text1.split("\\W+");
         Map<String, Integer> word = new HashMap<>();
         for (String w : words) {
@@ -42,7 +23,6 @@ public class Lab4Task4 {
         }
         System.out.println("Частота слов: " + word);
 
-        // Определение символов, содержащихся хотя бы в одной строке
         Set<Character> uniqChars = new HashSet<>();
         for (char c : text1.toCharArray()) {
             uniqChars.add(c);
